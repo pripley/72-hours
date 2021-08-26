cf8df68d0a83f921ebe16c29c965e85388b31d6b
Paul Ripley <paulripley@hotmail.com>
2021-08-26 19:55:01 -0400

    nasa updates

--- src/App.js
+++ src/App.js
@@ -1,33 +1,27 @@
-
-import './App.css';
-import React, { useState, useEffect } from 'react';
-import Nasa from './nasa/Nasa';
-import Weather from './weather/Weather';
-import Ticketmaster from './ticketmaster/Ticketmaster';
-
-
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import Nasa from "./nasa/Nasa";
+import Weather from "./weather/Weather";
+import Ticketmaster from "./ticketmaster/Ticketmaster";
 
 function App() {
-  const [location, setLocation] = useState({}) 
+  const [location, setLocation] = useState({});
 
   let locationGrabber = () => {
-       navigator.geolocation.getCurrentPosition((loc) => {
-        
-        setLocation(loc)
-        console.log(location)
-       })
-       
-
-  }
+    navigator.geolocation.getCurrentPosition((loc) => {
+      setLocation(loc);
+      console.log(location);
+    });
+  };
 
   useEffect(() => {
-    locationGrabber()
-  }, [])
+    locationGrabber();
+  }, []);
   return (
     <div className="App">
-      <Nasa location={location}/>
-      <Weather location={location}/>
-      <Ticketmaster location={location}/>
+      <Nasa location={location} />
+      <Weather location={location} />
+      <Ticketmaster location={location} />
     </div>
   );
 }
--- src/nasa/Nasa.js
+++ src/nasa/Nasa.js
@@ -3,27 +3,30 @@ import React, { useEffect, useState } from "react";
 const baseURL = "https://api.nasa.gov/planetary/earth/imagery";
 const key = "g2Aa7esA2tdERwMZ1r7P0RlHzNLvAfuvMLaONh9R";
 
-const Nasa = () => {
-  let lon = "86.1581";
-  let lat = "39.7684";
-  const [image, displayImage] = useState('');
+const Nasa = (props) => {
+  let lat = "1.5";
+  let lon = "100.75";
 
-  const fetchResults = () => {
-    let url = `${baseURL}?lat=${lat}&lon=${lon}&api_key=${key}`;
+  const [image, displayImage] = useState("");
+
+  const fetchResults = async () => {
+    let url = `${baseURL}?lon=${lon}&lat=${lat}&api_key=${key}`;
 
     fetch(url)
-      .then((res) => res.blob())      
+      .then((res) => res.blob())
       .then((image) => displayImage(URL.createObjectURL(image)))
       .catch((err) => console.log(err));
-      
   };
 
-  useEffect(fetchResults,[])
+  useEffect(fetchResults, []);
 
   return (
     <div className="main">
-      <div className="mainDiv">        
-        <p><img src={image} alt="" style={{ width: 300 }} /></p>
+      <div className="mainDiv">
+        <h2>NASA Landsat 8 image</h2>
+        <p>
+          <img src={image} alt="" style={{ width: 300 }} />
+        </p>
       </div>
     </div>
   );
